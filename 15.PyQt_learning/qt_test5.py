5dc355e3f0f8b0f27a859a98be20a93048769be9
firewang <wanglian2016@gmail.com>
2018-08-29 11:26:10 +0800

    message box 确认是否关闭窗口

---
+++ 15.PyQt_learning/qt_test5.py
@@ -0,0 +1,78 @@
+# -*- encoding: utf-8 -*-
+# @Version : 1.0  
+# @Time    : 2018/8/29 10:29
+# @Author  :  wanghuodong  
+# @note    : message box 确认是否关闭窗口
+
+import sys
+# QtWidgets包含创造经典桌面风格的用户界面提供了一套UI元素的类。
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QMessageBox
+# QtGui包含类窗口系统集成、事件处理、二维图形、基本成像、字体和文本。
+from PyQt5.QtGui import QIcon,QFont
+#QtCore:包含了核心的非GUI功能。此模块用于处理时间、文件和目录、各种数据类型、流、URL、MIME类型、线程或进程。
+from PyQt5.QtCore import QCoreApplication
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        # Example类继承自QWidget类,调用父类的构造方法
+        super().__init__()
+        # 界面绘制交给InitUI 方法
+        self.initUI()
+
+    def initUI(self):
+        #setFont()这个静态方法设置了用于提示框的字体。我们使用10px大小的SansSerif字体。
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        #创建提示框，我们调用了setTooltip() ， 为QWidget设置提示框，相当于对整个应用的一个提示
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        '''创建了一个按钮组件并且为它设置一个提示框。'''
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        '''设置按钮的大小，以及在窗口上的（相对）位置。setHint()方法给了按钮一个推荐的大小。'''
+        btn.resize(btn.sizeHint())
+        btn.move(20, 150)
+
+        '''创建 退出按钮, 并放置在顶层QWidget下 '''
+        quitbtn = QPushButton('Quit', self)
+        #设置提示框
+        quitbtn.setToolTip('This is a quit button 这是退出按键')
+        '''事件处理系统由信号&槽机制建立。如果我们点击了按钮，信号clicked被发送。槽可以是Qt内置的槽或Python 的一个方法调用。
+        QCoreApplication类包含了主事件循环；它处理和转发所有事件。instance()方法给我们返回一个实例化对象。
+        点击信号连接到quit()方法，将结束应用。  事件通信在两个对象之间进行：发送者和接受者。发送者是按钮，接受者是应用对象。'''
+        quitbtn.clicked.connect(QCoreApplication.instance().quit)
+
+        '''设置按钮的大小，以及在窗口上的（相对）位置。setHint()方法给了按钮一个推荐的大小。'''
+        quitbtn.resize(btn.sizeHint())    #width,height
+        quitbtn.move(170, 150)     # x,y
+
+
+        '''三个方法都继承自QWidgets类。
+        setGeometry()方法的前两个参数定位了窗口的x轴和y轴位置。第三个参数是定义窗口的宽度，第四个参数是定义窗口的高度。
+        setWindowTitle() 设置了窗口的标题
+        我们创建了一个QIcon对象。QIcon对象接收一个我们要显示的图片路径作为参数。'''
+        self.setGeometry(300, 300, 300, 220)  #(x,y, width,height)
+        self.setWindowTitle('Tooltips')
+        self.setWindowIcon(QIcon('panda.png'))
+        self.show()
+
+    def closeEvent(self, event):
+        '''第一个字符串的内容被显示在标题栏上。
+        第二个字符串是对话框上显示的文本。
+        第三个参数指定了显示在对话框上的按钮集合。
+        最后一个参数是默认选中的按钮。这个按钮一开始就获得焦点。
+        返回值被储存在reply变量中。'''
+        reply = QMessageBox.question(self, 'Message退出提示框标题', "Are you sure to quit?退出提示", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    ex = Example()
+
+    sys.exit(app.exec_())
