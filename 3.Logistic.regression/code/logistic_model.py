8941aa7bd9aa3e2c36049064d9773ab9995fb0a7
firewang <wanglian2016@gmail.com>
2019-07-26 17:16:31 +0800

    逻辑回归简单优化

--- 3.Logistic.regression/code/logistic_model.py
+++ 3.Logistic.regression/code/logistic_model.py
@@ -8,13 +8,19 @@ from sklearn import datasets
 from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 import numpy as np
-import pyecharts as pe
+import pyecharts as pe  # pyecharts==0.5.11
+import pandas as pd
+import warnings
+
+warnings.filterwarnings('ignore')
+
 
 # 鸢尾花 setosa, versicolor, virginica
 # 萼片sepal 长度、宽度， 花瓣petal 长度、宽度
 def load_data():
     iris = datasets.load_iris()  # 分层采样 stratify
-    return model_selection.train_test_split(iris.data, iris.target, test_size=0.25, random_state=0, stratify=iris.target)
+    return model_selection.train_test_split(iris.data, iris.target, test_size=0.25, random_state=0,
+                                            stratify=iris.target)
 
 
 def test_LogisticRegression(*data):
@@ -22,7 +28,10 @@ def test_LogisticRegression(*data):
     regr = LogisticRegression()
     regr.fit(X_train, y_train)
     print("Coefficients:{coefficients} , intercept:{intercept}".format(coefficients=regr.coef_,
-                                                                             intercept=regr.intercept_))
+                                                                       intercept=regr.intercept_))
+    coefficients = pd.DataFrame(regr.coef_, columns=[f"coeff_{col}" for col in range(pd.DataFrame(X_train).shape[1])])
+    coefficients.loc[:, "intercept"] = pd.Series(regr.intercept_)
+    print(coefficients)
     print("score:{score:>+6.2f}".format(score=regr.score(X_test, y_test)))
 
 
@@ -30,29 +39,33 @@ X_train, X_test, y_train, y_test = load_data()
 test_LogisticRegression(X_train, X_test, y_train, y_test)
 
 
-
 ########分类策略 multi_class 的影响
 def test_LogisticRegression_multinomial(*data):
     X_train, X_test, y_train, y_test = data
-    regr = LogisticRegression(multi_class='multinomial',solver='lbfgs')
+    regr = LogisticRegression(multi_class='multinomial', solver='lbfgs')
     regr.fit(X_train, y_train)
     print("Coefficients:{coefficients} , intercept:{intercept}".format(coefficients=regr.coef_,
-                                                                             intercept=regr.intercept_))
+                                                                       intercept=regr.intercept_))
+    coefficients = pd.DataFrame(regr.coef_, columns=[f"coeff_{col}" for col in range(pd.DataFrame(X_train).shape[1])])
+    coefficients.loc[:, "intercept"] = pd.Series(regr.intercept_)
+    print(coefficients)
     print("score:{score:>+6.2f}".format(score=regr.score(X_test, y_test)))
+
 
 test_LogisticRegression_multinomial(X_train, X_test, y_train, y_test)
 
+
 def test_LogisticRegression_C(*data):
     X_train, X_test, y_train, y_test = data
-    Cs = np.logspace(-2,4,num=100)  #等比数列
+    Cs = np.logspace(-2, 4, num=100)  # 等比数列
     scores = []
     for c in Cs:
-        regr = LogisticRegression(multi_class='multinomial',solver='lbfgs',C=c)
+        regr = LogisticRegression(multi_class='multinomial', solver='lbfgs', C=c)
         regr.fit(X_train, y_train)
-        scores.append(regr.score(X_test,y_test))
+        scores.append(regr.score(X_test, y_test))
     line = pe.Line("LogisticRegression_C")
-    line.add("LogisticRegression_C",Cs,scores,yaxis_min=0.8,yaxis_max=1,
-             xaxis_name="C",yaxis_name='Score',yaxis_name_gap=50)
+    line.add("LogisticRegression_C", Cs, scores, yaxis_min=0.8, yaxis_max=1,
+             xaxis_name="C", yaxis_name='Score', yaxis_name_gap=50)
     line.render("../output/LogisticRegression_C.html")
 
 
